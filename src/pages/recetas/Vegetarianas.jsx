faf9c1659a5405061bd6e72f508f3ecce8fc5bad
jaimezzzapata <medicenzapata@gmail.com>
2024-09-17 20:58:17 -0500

    Implementación de recorrido de arreglo de datos en componente Vegetarianas

--- src/pages/recetas/Vegetarianas.jsx
+++ src/pages/recetas/Vegetarianas.jsx
@@ -1,6 +1,20 @@
+import { recetasVegetarianas } from "../../data/recetas"
+
 const Vegetarianas = () => {
   return (
-    <div>Vegetarianas</div>
+    <div>
+      {
+        recetasVegetarianas.map((receta) => (
+          <section>
+            {/* <p>Id: {receta.id}</p> */}
+            <p>Nombre: {receta.nombre}</p>
+            <p>Descripcion: {receta.descripcion}</p>
+            <p>Dificultad: {receta.dificultad}</p>
+            <p>Tiempo: {receta.tiempo}</p>
+          </section>
+        ))
+      }
+    </div>
   )
 }
 
