f11f157a17c0d578ddc838e3e9e300c002985cb3
jaimezzzapata <medicenzapata@gmail.com>
2024-09-17 19:26:28 -0500

    Se crea estructura del Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,20 @@
+import Header from "../components/Header"
+import { Link } from 'react-router-dom'
+import './Dashboard.css'
+
 const Dashboard = () => {
-    return(
+    return (
         <main>
-            <h1>Esto es el panel de control</h1>
+            <Header />
+            <section className="panel-control">
+                <nav>
+                    <Link>Recetas Vegetariasn</Link>
+                    <Link>Recetas No Vegetarianas</Link>
+                </nav>
+                <section>
+
+                </section>
+            </section>
         </main>
     )
 }
