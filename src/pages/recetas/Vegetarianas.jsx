01ff249313aceea8f79999e0cda9dec7df46a362
jaimezzapata <zapataval2304@gmail.com>
2024-11-12 19:18:29 -0500

    Se implemente método de eliminar receta

--- src/pages/recetas/Vegetarianas.jsx
+++ src/pages/recetas/Vegetarianas.jsx
@@ -1,41 +1,74 @@
-import { Link } from "react-router-dom"
-import './Vegetarianas.css'
-import { useEffect, useState } from "react"
-let urlRecetas = 'http://localhost:3000/vegetarianas'
+import { Link } from "react-router-dom";
+import "./Vegetarianas.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Swal from "sweetalert2";
+let urlRecetas = "http://localhost:3000/vegetarianas";
 
 const Vegetarianas = () => {
-  const [recetasVegetarianas, setRecetasVegetarianas] = useState([])
+  const [recetasVegetarianas, setRecetasVegetarianas] = useState([]);
 
   function consultarRecetas() {
     fetch(urlRecetas)
-      .then(response => response.json())
-      .then(json => setRecetasVegetarianas(json))
-      .catch(error => console.log(error))
+      .then((response) => response.json())
+      .then((json) => setRecetasVegetarianas(json))
+      .catch((error) => console.log(error));
   }
   useEffect(() => {
-    consultarRecetas()
-  }, [])
+    consultarRecetas();
+  }, []);
+
+  function eliminarReceta(id) {
+    axios.delete(urlRecetas + "/" + id);
+    consultarRecetas();
+  }
+
+  function confirmarEliminarReceta(id, nombre) {
+    Swal.fire({
+      title: "Está seguro?",
+      text: "Se va a eliminar la receta " + nombre,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, eliminar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        eliminarReceta(id);
+        Swal.fire({
+          title: "Elminado!",
+          text: "La receta se eliminó de forma correcta.",
+          icon: "success",
+        });
+      }
+    });
+  }
 
   return (
     <div className="cards">
-      {
-        recetasVegetarianas.map((receta) => (
-          <section key={receta.id}>
-            {/* <p>Id: {receta.id}</p> */}
-            <p>Nombre: {receta.nombre}</p>
-            <p>Descripcion: {receta.descripcion}</p>
-            <p>Dificultad: {receta.dificultad}</p>
-            <p>Tiempo: {receta.tiempo}</p>
-            <div>
-              <button className="eliminar">Eliminar</button>
-              <Link to={'/editar-receta-vegetariana'} className="editar">Editar</Link>
-              <Link className="detalle">Detalles</Link>
-            </div>
-          </section>
-        ))
-      }
+      {recetasVegetarianas.map((receta) => (
+        <section key={receta.id}>
+          {/* <p>Id: {receta.id}</p> */}
+          <p>Nombre: {receta.nombre}</p>
+          <p>Descripcion: {receta.descripcion}</p>
+          <p>Dificultad: {receta.dificultad}</p>
+          <p>Tiempo: {receta.tiempo}</p>
+          <div>
+            <button
+              onClick={() => confirmarEliminarReceta(receta.id, receta.nombre)}
+              className="eliminar"
+            >
+              Eliminar
+            </button>
+            <Link to={"/editar-receta-vegetariana"} className="editar">
+              Editar
+            </Link>
+            <Link className="detalle">Detalles</Link>
+          </div>
+        </section>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Vegetarianas
+export default Vegetarianas;
